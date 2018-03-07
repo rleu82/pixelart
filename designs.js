0b1d4aa7659ccd2490b6835bd0a38d3ac27e60e5
Robert Leu <Robert@Roberts-MBP.fios-router.home>
2018-03-06 22:27:14 -0800

    setup functions

--- designs.js
+++ designs.js
@@ -2,9 +2,20 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function newGrid(){
+	$('pixelCanvas')
 }
+function makeGrid(numColumns, numRows) {
+    const columnsRowsTable = '';
+    const numColumns = $('#inputWeight').val();
+    const numRows = $('#inputHeight').val();
+    for (x = 1; x <= numColumns; x++) {
+		columnsRowsTable += '<tr>';
+		for (y = 1; y <= numRows; y++) {
+			columnsRowsTable += '<td>&nbsp;</td>';
+		}
+		columnsRowsTable += '</tr>';	
+	}
+    //$('columnsRowsTable').appendTo('#pixelCanvas');
+console.log(columnsRowsTable);
+};
