d8894c957d20f8159e0d0b5a6f34c98ce65f2201
Robert Leu <Robert@Roberts-MBP.fios-router.home>
2018-03-27 01:11:01 -0700

    created basic functions for drawing

--- designs.js
+++ designs.js
@@ -1,13 +1,16 @@
-// Once document is loaded makeGrid function activates
-// and listens for new submits
+// Once document is loaded
 $(document).ready(function(){
-	$('#gridSize').submit(function(evt) {
+	makeGrid();
+	let mouseDown = false;
+	$('#gridSize').submit(function(event) {
+	let mouseDown = false;
 	newGrid();
 	makeGrid();
-	evt.preventDefault();
+	event.preventDefault();
 	});
-	
+
 });
+
 
 // Resets grid by removing all rows(tr) and colums(td) from
 // pixelCanvas. Children elements removed.
@@ -15,18 +18,58 @@ function newGrid(){
 	$('pixelCanvas').children().remove();
 }
 
-/* Creates new grid from values inputed by adding tr and td
+
+/* Creates new grid from values inputed by adding tr and td,
 then adding the html into pixelCanvas table */
 function makeGrid() {
-    var columnsRowsTable = '';
-    var numColumns = $('#inputWeight').val();
-    var numRows = $('#inputHeight').val();
-    for (x = 1; x <= numColumns; x++) {
+    const numColumns = $('#inputWeight').val();
+	const numRows = $('#inputHeight').val();
+	let columnsRowsTable = '';
+	
+	for (x = 1; x <= numColumns; x++) {
 		columnsRowsTable += '<tr>';
+
 		for (y = 1; y <= numRows; y++) {
 			columnsRowsTable += '<td>&nbsp;</td>';
 		}
 		columnsRowsTable += '</tr>';	
 	}
+
 	$('#pixelCanvas').html(columnsRowsTable);
 };
+
+
+// Confirm mouseDown = true
+$('#pixelCanvas').delegate('td', 'mousedown', function() {
+	mouseDown = true;
+});
+
+// Confirm mouseDown = false
+$('#pixelCanvas').delegate('td', 'mouseup', function() {
+	mouseDown = false;
+});
+
+// Click to paint
+$('#pixelCanvas').delegate('td', 'click', function() {
+	const colorSelected = $('#colorPicker').val(); // chosen color
+	$(this).css('background-color', colorSelected); // current td
+	$('#pixelHtml').val('<table style="border-collapse: collapse; margin: 0 auto; width: 100%;">' + $(pixelCanvas).html() + '</table>'
+);
+});
+
+// If mouseDown = true paint td cells by moving mouse
+$('#pixelCanvas').delegate('td', 'mouseenter', function() {
+	if (mouseDown) { // if mouseDown = true
+		const colorSelected = $('#colorPicker').val(); // chosen color
+		$(this).css('background-color', colorSelected); // current td
+	}
+});
+
+// Erase td cell with double click
+$('#pixelCanvas').delegate('td', 'dblclick', function() {
+	$(this).css('background-color', 'none'); // current td                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+});
+
+/*
+$('#pixelHtml').val('<table style="border-collapse: collapse; margin: 0 auto;' + $(pixelCanvas).html() + '</table>'
+); */
